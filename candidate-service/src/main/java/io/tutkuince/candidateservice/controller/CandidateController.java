a35f7964fd108cfc5a80e02e107c2f7ad56b0499
Tutku Ince <tutku.ince@outlook.com>
2024-01-20 22:04:02 +0300

    service and controller updates

--- candidate-service/src/main/java/io/tutkuince/candidateservice/controller/CandidateController.java
+++ candidate-service/src/main/java/io/tutkuince/candidateservice/controller/CandidateController.java
@@ -1,6 +1,7 @@
 package io.tutkuince.candidateservice.controller;
 
 import io.tutkuince.candidateservice.client.JobClient;
+import io.tutkuince.candidateservice.dto.CandidateDetailsDto;
 import io.tutkuince.candidateservice.dto.CandidateDto;
 import io.tutkuince.candidateservice.service.CandidateService;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,7 @@ public class CandidateController {
     }
 
     @GetMapping("{id}")
-    public Mono<CandidateDto> findById(@PathVariable String id) {
+    public Mono<CandidateDetailsDto> findById(@PathVariable String id) {
         return this.candidateService.candidateById(id);
     }
 
--- candidate-service/src/main/java/io/tutkuince/candidateservice/service/CandidateService.java
+++ candidate-service/src/main/java/io/tutkuince/candidateservice/service/CandidateService.java
@@ -1,5 +1,7 @@
 package io.tutkuince.candidateservice.service;
 
+import io.tutkuince.candidateservice.client.JobClient;
+import io.tutkuince.candidateservice.dto.CandidateDetailsDto;
 import io.tutkuince.candidateservice.dto.CandidateDto;
 import io.tutkuince.candidateservice.repository.CandidateRepository;
 import io.tutkuince.candidateservice.util.EntityDtoUtil;
@@ -10,9 +12,11 @@ import reactor.core.publisher.Mono;
 @Service
 public class CandidateService {
     private final CandidateRepository candidateRepository;
+    private final JobClient client;
 
-    public CandidateService(CandidateRepository candidateRepository) {
+    public CandidateService(CandidateRepository candidateRepository, JobClient client) {
         this.candidateRepository = candidateRepository;
+        this.client = client;
     }
 
     public Flux<CandidateDto> allCandidates() {
@@ -21,10 +25,11 @@ public class CandidateService {
                 .map(EntityDtoUtil::toCandidateDto);
     }
 
-    public Mono<CandidateDto> candidateById(String id) {
+    public Mono<CandidateDetailsDto> candidateById(String id) {
         return this.candidateRepository
                 .findById(id)
-                .map(EntityDtoUtil::toCandidateDto);
+                .map(EntityDtoUtil::toCandidateDetailsDto)
+                .flatMap(this::addRecommendedJobs);
     }
 
     public Mono<CandidateDto> save(Mono<CandidateDto> dto) {
@@ -32,5 +37,11 @@ public class CandidateService {
                 .map(EntityDtoUtil::toCandidate)
                 .flatMap(this.candidateRepository::save)
                 .map(EntityDtoUtil::toCandidateDto);
+    }
+
+    private Mono<CandidateDetailsDto> addRecommendedJobs(CandidateDetailsDto dto) {
+        return this.client.getRecommendedJobs(dto.getSkills())
+                .doOnNext(dto::setRecommendedJobs)
+                .thenReturn(dto);
     }
 }
