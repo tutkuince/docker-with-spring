3129e43bf9b5884746d46cf3741a4a10a8df75b2
Tutku Ince <tutku.ince@outlook.com>
2024-01-20 22:54:54 +0300

    new test methods has been added

--- candidate-service/src/test/java/io/tutkuince/candidateservice/CandidateServiceIT.java
+++ candidate-service/src/test/java/io/tutkuince/candidateservice/CandidateServiceIT.java
@@ -34,7 +34,9 @@ class CandidateServiceIT extends BaseTest {
                 .expectStatus().is2xxSuccessful()
                 .expectBody()
                 .jsonPath("$.id").isEqualTo(1)
-                .jsonPath("$.name").isEqualTo("sam");
+                .jsonPath("$.name").isEqualTo("sam")
+                .jsonPath("$.skills.size()").isEqualTo(2)
+                .jsonPath("$.recommendedJobs.size()").isEqualTo(2);
     }
 
     @Test
@@ -48,6 +50,32 @@ class CandidateServiceIT extends BaseTest {
                 .expectBody()
                 .jsonPath("$.id").isNotEmpty()
                 .jsonPath("$.name").isEqualTo("tutku");
+    }
+
+    @Test
+    void jobServiceReturns4xx() {
+        this.client.get()
+                .uri("/candidate/2")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(2)
+                .jsonPath("$.name").isEqualTo("jake")
+                .jsonPath("$.skills.size()").isEqualTo(1)
+                .jsonPath("$.recommendedJobs").isEmpty();
+    }
+
+    @Test
+    void jobServiceReturns5xx() {
+        this.client.get()
+                .uri("/candidate/3")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(3)
+                .jsonPath("$.name").isEqualTo("mike")
+                .jsonPath("$.skills.size()").isEqualTo(1)
+                .jsonPath("$.recommendedJobs").isEmpty();
     }
 
 }
